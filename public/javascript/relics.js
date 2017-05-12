26288b316a81b70a59295cbe2881e75ba2edb3e2
Thomas <tazfrison@gmail.com>
2017-05-12 12:52:29 -0700

    Removed unnecessary relic controller since logic was moved into the class

--- public/javascript/relics.js
+++ public/javascript/relics.js
@@ -22,51 +22,6 @@ angular.module('relics', ["data"])
 		}],
 		templateUrl: "templates/relics.template.html"
 	})
-
-	.controller("RelicCtrl", ["$scope", "DataService", "$q", function($scope, DataService, $q){
-		$scope.completion = () => ({
-			ducats: [0, 0, 0, 0],
-			newPart: [0, 0, 0, 0],
-			length: "0%"});
-		
-		let chances = [
-			[.253, .11, .02],
-			[.233, .13, .04],
-			[.200, .17, .06],
-			[.167, .20, .10]
-		];
-		
-		DataService.service.then(service => {
-			/*$scope.completion = () => {
-				let owned = 0;
-				let required = 0;
-				let ducats = [0, 0, 0, 0];
-				let getNew = [0, 0, 0, 0];
-				$scope.relic.rewards.forEach(reward => {
-					let part = service.getPart(reward.partId);
-					required += part.required;
-					owned += Math.min(part.owned + part.blueprints, part.required);
-					if(part.owned + part.blueprints >= part.required)
-					{
-						for(let i = 0; i < 4; ++i){
-							ducats[i] += part.ducats * chances[i][reward.rarity];
-						}
-					}
-					else{
-						for(let i = 0; i < 4; ++i){
-							getNew[i] += chances[i][reward.rarity] * 100;
-						}
-					}
-				});
-				
-				return {
-					ducats: ducats,
-					newPart: getNew,
-					length: (100 * owned / required) + "%"
-				}
-			};*/
-		});
-	}])
 	
 	.controller("RewardCtrl", ["$scope", "DataService", function($scope, DataService){
 		$scope.part = null;
